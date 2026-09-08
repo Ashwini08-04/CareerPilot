12c4112dfb79efbe288a5e863319e88a8f214fef
Ashwini Sonawane <sonawaneashwini073@gmail.com>
2026-09-08 11:15:08 +0530

    Connect frontend to production backend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://careerpilot-n4ys.onrender.com/api";
 
 export async function apiRequest(endpoint, options = {}) {
     const token = localStorage.getItem("token");
